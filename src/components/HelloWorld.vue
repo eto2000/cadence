0555a4e9d15113b908443981c604d7793d9f2e07
steve <soocoolkim@gmail.com>
2023-09-27 15:59:14 +0900

    1.0

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,18 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <img @click="onClick" alt="Vue logo" src="../assets/logo.png">
+
+    <h2 >왼발에 맞춰 <span style="color: red">로고</span>를 터치하세요</h2>
+
+    <br>
+    <h2>데이터가 많을수록 정확합니다.</h2>
+
+    <br>
+    <h1>{{ result }}</h1>
+
+    <br>
+    <h1 @click="onInit">초기화</h1>
+
   </div>
 </template>
 
@@ -35,6 +21,42 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return {
+      result: 0,
+      start: null,
+      last: null,
+      count: 0,
+    }
+  },
+  methods: {
+    onClick(){
+      console.log('onClick')
+
+      if(this.start == null){
+        this.start = new Date().getTime();
+        return;
+      } 
+
+      this.count++;
+
+      this.last = new Date().getTime();
+
+      const diff = this.last - this.start;
+      // console.log('this.last - this.start>', diff)
+      // console.log('this.count ', this.count)
+
+      this.result = Math.floor(this.count * 1000 * 60 * 2 / diff)
+    },
+    onInit(){
+      console.log('onInit')
+
+      this.start = null;
+      this.last = null;
+      this.count = 0;
+      this.result = 0;
+    }
   }
 }
 </script>
